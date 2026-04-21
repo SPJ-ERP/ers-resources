219c33ecb11ff9487d1a991d9caeb9cd61420802
devit <60412293+devit7@users.noreply.github.com>
2026-04-21 15:31:46 +0700

    fix: url update

---
+++ constant/url/dashboard.ts
@@ -0,0 +1,5 @@
+import { ALL_MODULE_PORTS } from "../port/all-module-port";
+
+const HOST = "http://localhost";
+export const DASHBOARD_LOGIN_URL = `${HOST}:${ALL_MODULE_PORTS.DASHBOARD}/login`;
+export const DASHBOARD_DASHBOARD_URL = `${HOST}:${ALL_MODULE_PORTS.DASHBOARD}`;
